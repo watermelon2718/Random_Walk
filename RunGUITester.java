afd754d8640839a7ef4bdf1cf795c67b1034f822
watermelon2718 <ellyntran@my.cwi.edu>
2025-04-25 12:38:24 -0600

    Add files via upload

---
+++ RunGUITester.java
@@ -0,0 +1,8 @@
+import edu.cwi.randomwalk.RandomWalkGUI;
+
+public class RunGUITester {
+	public static void main(String[] args) {
+		RandomWalkGUI.main(args);		
+	}
+
+}
---
+++ RunUnitTester.java
@@ -0,0 +1,7 @@
+import edu.cwi.randomwalk.RandomWalkUnitTester;
+
+public class RunUnitTester {
+	public static void main(String[] args) {
+		RandomWalkUnitTester.main(args);
+	}
+}
